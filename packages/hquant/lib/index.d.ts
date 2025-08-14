2e9ad2d9b3ffd746560c810f59f2987df1c61d5e
hu-bo <751968840@qq.com>
2025-08-14 08:29:40 +0800

    hquant: 0.0.2

--- packages/hquant/lib/index.d.ts
+++ packages/hquant/lib/index.d.ts
@@ -1,8 +1,12 @@
 export { Quant } from './Quant.js';
 export { CircularQueue } from './common/CircularQueue.js';
 export { AverageQueue } from './common/AverageQueue.js';
+export { GoldenRatioCalculator } from './common/GoldenRatioCalculator.js';
+export { SharedBufferQueue } from './common/SharedBufferQueue.js';
 export { Indicator, Kline, KlineIn, OrderSide, PositionSide, Signal, Strategy, numberInString } from './interface.js';
 export { KlineState, Slope } from './indicator/slope.js';
 export { BOLL } from './indicator/boll.js';
 export { MA } from './indicator/ma.js';
 export { RSI } from './indicator/rsi.js';
+export { ATR } from './indicator/atr.js';
+export { VRI } from './indicator/vri.js';
